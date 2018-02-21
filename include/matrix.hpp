b033e9b17347564699d8c5410055bfeef5447520
Roman Khomenko <romankhomenko1995@gmail.com>
2018-02-21 20:25:58 +0300

    Migration to std::vector and bugfix

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -1,9 +1,9 @@
 #ifndef CHISLAKI_MATRIX_HPP_
 #define CHISLAKI_MATRIX_HPP_
 
-#include <cstring>
-#include <memory>
+#include <iostream>
 #include <numeric>
+#include <vector>
 
 namespace chislaki {
 
@@ -13,147 +13,179 @@ class bad_size {};
 template <class T>
 class matrix {
 public:
-    /*
-     * Constructors
-     */
+    using index_type = std::size_t;
+    using value_type = T;
+    using reference = T&;
+    using const_reference = const T&;
+    using iterator = T*;
+    using const_iterator = const T*;
 
-    matrix(std::size_t rows, std::size_t columns)
-        : rows_{rows}, columns_{columns} {
+    matrix(index_type rows, index_type columns)
+        : rows_{rows}, columns_{columns}, data_(rows * columns) {
         if (rows == 0 || columns == 0) {
             throw bad_size{};
         }
-        data_ = std::shared_ptr<T>{new T[rows * columns],
-                                   std::default_delete<T[]>()};
     }
 
-    matrix(std::size_t rows, std::size_t columns, const T* data)
-        : matrix(rows, columns) {
-        std::memcpy(data_.get(), data, rows * columns);
-    }
+    matrix(index_type rows, index_type columns, const std::vector<T>& data)
+        : rows_{rows}, columns_{columns}, data_{data} {}
 
     matrix(const matrix& matr) = default;
 
     matrix(matrix&& matr) : rows_{matr.rows_}, columns_{matr.columns_} {
-        data_ = nullptr;
-        std::swap(data_, matr.data_);
+        data_ = matr.data_;
+        matr.data_ = std::vector<T>();
     }
 
-    /*
-     * Size
-     */
+    index_type columns() const noexcept { return columns_; }
 
-    std::size_t colums() const noexcept { return columns_; }
+    index_type rows() const noexcept { return rows_; }
 
-    std::size_t rows() const noexcept { return rows_; }
+    const_iterator data() const noexcept { return data_.data(); }
 
-    /*
-     * Indexing
-     */
+    iterator data() noexcept { return data_.data(); }
 
-    const T& operator()(std::size_t row, std::size_t column) const noexcept {
-        return data_[row * rows() + column];
+    const_iterator begin() const noexcept { return &data_[0]; }
+
+    iterator begin() noexcept { return &data_[0]; }
+
+    const_iterator end() const noexcept {
+        return &data_[rows() * columns() - 1];
     }
 
-    const T& at(std::size_t row, std::size_t column) const {
-        if (!(row < rows() && column < colums())) {
+    iterator end() noexcept { return &data_[rows() * columns() - 1]; }
+
+    const_reference operator()(index_type row, index_type column) const
+        noexcept {
+        return data_[row * columns() + column];
+    }
+
+    const_reference at(index_type row, index_type column) const {
+        if (!(row < rows() && column < columns())) {
             throw bad_index{};
         }
-        return data_[row * rows() + column];
+        return data_[row * columns() + column];
     }
 
-    T& operator()(std::size_t row, std::size_t column) noexcept {
-        return data_[row * rows() + column];
+    reference operator()(index_type row, index_type column) noexcept {
+        return data_[row * columns() + column];
     }
 
-    T& at(std::size_t row, std::size_t column) {
+    reference at(index_type row, index_type column) {
         if (!(row << rows() && column < rows())) {
             throw bad_index{};
         }
-        return data_[row * rows() + column];
+        return data_[row * columns() + column];
     }
 
-    /*
-     * Algorithms
-     */
+    T row_max_value(index_type row) const { return row_max(row).second; }
 
-    T row_max_value(std::size_t row) const { return row_max(row).first; }
+    T row_max_index(index_type row) const { return row_max(row).first; }
 
-    T row_max_index(std::size_t row) const { return row_max(row).second; }
-
-    std::pair<std::size_t, T> row_max(std::size_t row) const {
-        return column_row_max_min(
-            std::numeric_limits<T>::min(), row, colums(),
-            [this](std::size_t index, std::size_t current_index) {
-                return at(index, current_index);
-            },
+    std::pair<index_type, T> row_max(index_type row) const {
+        return row_max_min(
+            std::numeric_limits<T>::min(), row,
             [this](T left, T right) { return left > right; });
     }
 
-    T col_max_value(std::size_t column) const { return row_max(column).first; }
-
-    T col_max_index(std::size_t column) const {
+    T col_max_value(index_type column) const {
         return column_max(column).second;
     }
 
-    std::pair<std::size_t, T> column_max(std::size_t column) const {
-        return column_row_max_min(
-            std::numeric_limits<T>::min(), column, rows(),
-            [this](std::size_t index, std::size_t current_index) {
-                return at(current_index, index);
-            },
-            [this](T left, T right) { return left > right; });
+    T col_max_index(index_type column) const {
+        return column_max(column).first;
     }
 
-    T row_min_value(std::size_t row) const { return row_min(row).first; }
-
-    T row_min_index(std::size_t row) const { return row_min(row).second; }
-
-    std::pair<std::size_t, T> row_min(std::size_t row) const {
-        return column_row_max_min(
-            std::numeric_limits<T>::max(), row, colums(),
-            [this](std::size_t index, std::size_t current_index) {
-                return at(index, current_index);
-            },
-            [this](T left, T right) { return left < right; });
+    std::pair<index_type, T> column_max(index_type index) const {
+        return column_max_min(std::numeric_limits<T>::min(), index,
+                              [this](T left, T right) { return left > right; });
     }
 
-    T col_min_value(std::size_t column) const { return row_min(column).first; }
+    T row_min_value(index_type row) const { return row_min(row).second; }
 
-    T col_min_index(std::size_t column) const {
+    T row_min_index(index_type row) const { return row_min(row).first; }
+
+    std::pair<index_type, T> row_min(index_type row) const {
+        return row_max_min(std::numeric_limits<T>::max(), row,
+                           [this](T left, T right) { return left < right; });
+    }
+
+    T col_min_value(index_type column) const {
         return column_min(column).second;
     }
 
-    std::pair<std::size_t, T> column_min(std::size_t column) const {
-        return column_row_max_min(
-            std::numeric_limits<T>::max(), column, rows(),
-            [this](std::size_t index, std::size_t current_index) {
-                return at(current_index, index);
-            },
-            [this](T left, T right) { return left < right; });
+    T col_min_index(index_type column) const {
+        return column_min(column).first;
+    }
+
+    std::pair<index_type, T> column_min(index_type column) const {
+        return column_max_min(std::numeric_limits<T>::max(), column,
+                              [this](T left, T right) { return left < right; });
+    }
+
+    void swap_rows(std::size_t row1, std::size_t row2) {
+        // TODO: Implement!
     }
 
 private:
-    template <class F1, class F2>
-    std::pair<std::size_t, T> column_row_max_min(T start_value,
-                                                 std::size_t index,
-                                                 std::size_t max_index,
-                                                 F1&& get,
-                                                 F2&& compare) {
+    template <class P>
+    std::pair<std::size_t, T> column_max_min(value_type start_value,
+                                             index_type index,
+                                             P&& compare) const {
         T current_value = start_value;
         std::size_t current_index = 0;
-        for (std::size_t i = 0; i < max_index; i++) {
-            if (compare(get(index, i), current_value)) {
-                current_value = get(i);
+        for (std::size_t i = 0; i < rows(); i++) {
+            if (compare(at(i, index), current_value)) {
+                current_value = at(i, index);
                 current_index = i;
             }
         }
         return std::make_pair(current_index, current_value);
     }
 
-    std::size_t rows_;
-    std::size_t columns_;
-    std::shared_ptr<T> data_;
+    template <class P>
+    std::pair<std::size_t, T> row_max_min(value_type start_value,
+                                          index_type index,
+                                          P&& compare) const {
+        T current_value = start_value;
+        std::size_t current_index = 0;
+        for (std::size_t j = 0; j < columns(); j++) {
+            if (compare(at(index, j), current_value)) {
+                current_value = at(index, j);
+                current_index = j;
+            }
+        }
+        return std::make_pair(current_index, current_value);
+    }
+
+    index_type rows_;
+    index_type columns_;
+    std::vector<T> data_;
 };  // class matrix
+
+template <class T>
+std::ostream& operator<<(std::ostream& os, const matrix<T>& matr) {
+    for (std::size_t i = 0; i < matr.rows(); i++) {
+        for (std::size_t j = 0; j < matr.columns(); j++) {
+            os << matr(i, j) << " ";
+        }
+        os << std::endl;
+    }
+
+    return os;
+}
+
+template <class T>
+std::istream& operator>>(std::istream& is, matrix<T>& matr) {
+    for (std::size_t i = 0; i < matr.rows(); i++) {
+        for (std::size_t j = 0; j < matr.columns(); j++) {
+            is >> matr(i, j);
+        }
+    }
+
+    return is;
+}
+
 }  // namespace chislaki
 
 #endif  // CHISLAKI_MATRIX_HPP_
